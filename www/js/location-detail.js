192e163665954eb5191c11a0c3717c39fcf7a603
licod1997 <licod1997@gmail.com>
2018-03-23 16:52:59 +0900

    Upgrade Materializecss to 1.0 beta version Adding location detail html

---
+++ www/js/location-detail.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var instance = M.Carousel.init({
+        fullWidth: true
+        , indicators: true
+    });
+    // Or with jQuery
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+        , indicators: false
+    });
+});
