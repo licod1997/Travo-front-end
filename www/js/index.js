eb209e79fb8f67dba2693307fcc32fc46e2d4adc
licod1997 <licod1997@gmail.com>
2018-03-24 19:24:08 +0900

    index.html

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,36 @@
 $(document).ready(function () {
-    $.ajax({
-        url: 'http://localhost:8080/auth',
-        type: 'GET',
-        success: function (result) {
-            console.log("123");
-        },
-        error: function (xhr, textStatus, errorThrown) {
-            window.location = 'login.html'
-        }
+    //    $.ajax({
+    //        url: 'http://localhost:8080/auth',
+    //        type: 'GET',
+    //        success: function (result) {
+    //            console.log("123");
+    //        },
+    //        error: function (xhr, textStatus, errorThrown) {
+    //            window.location = 'login.html'
+    //        }
+    //    });
+
+    var inputField = $('.input-field');
+    var notInputField = $('.input-field').siblings();
+    var input = $('#search');
+    var closeButton = $('.close-button');
+
+    $('.tabs').tabs();
+    $('.sidenav').sidenav();
+
+    $('.searchnav-trigger').click(function () {
+        notInputField.hide();
+        inputField.show();
+        input.focus();
     });
 
-    var closeButton = $('#close-button');
+    closeButton.click(function () {
+        inputField.hide();
+        notInputField.show();
+    });
 
-    closeButton.hide();
-
-    $('#search').hide();
-
-    $(".button-collapse").sideNav();
-
+    input.blur(function () {
+        inputField.hide();
+        notInputField.show();
+    });
 });
